ea7272db892b328c5c874657c7e32541477f87be
Miguel Cobian <miguelcobian99@gmail.com>
2024-04-04 17:43:15 -0600

    Feat. Add BudgetForm

---
+++ src/components/BudgetForm.tsx
@@ -0,0 +1,41 @@
+import { ChangeEvent, useMemo, useState } from "react"
+
+export const BudgetForm = () => {
+
+    const [budget, setBudget] = useState(0);
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+
+
+
+        setBudget(+e.target.value);
+    }
+
+    const isValid = useMemo(() => {
+        return isNaN(budget) || budget <= 0;
+    }, [budget]);
+
+    return (
+        <form className="space-y-5">
+            <div className="flex flex-col space-y-5">
+                <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">Definir presupuesto</label>
+
+                <input
+                    id="budget"
+                    type="number"
+                    className="w-full bg-white border-gray-200 p-2"
+                    placeholder="Define tu presupuesto"
+                    name="budget"
+                    value={budget}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <input
+                type="submit"
+                value="Definir presupuesto"
+                className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase"
+            />
+        </form>
+    )
+}
