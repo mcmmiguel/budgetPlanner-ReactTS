4dd9088e5b86648473047709ae428c1db56739a8
Miguel Cobian <miguelcobian99@gmail.com>
2024-04-08 22:01:48 -0600

    Feat. Show dynamic budget

--- src/components/AmountDisplay.tsx
+++ src/components/AmountDisplay.tsx
@@ -8,8 +8,8 @@ type AmountDisplayProps = {
 export const AmountDisplay = ({ label, amount }: AmountDisplayProps) => {
     return (
         <p className="text-2xl text-blue-600 font-bold">
-            {label && `${label}:`}
-            <span className="font-black">{formatCurrency(amount)}</span>
+            {label && `${label}: `}
+            <span className="font-black text-black">{formatCurrency(amount)}</span>
         </p>
     )
 }
